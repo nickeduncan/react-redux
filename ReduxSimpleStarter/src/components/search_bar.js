08d307f7bb10d8f95cf0f9b1315fbe997230383f
Nick Duncan <nickeduncan@gmail.com>
2017-04-28 11:41:04 -0400

    began tracking state of searchbar input

--- ReduxSimpleStarter/src/components/search_bar.js
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
-class SearchBar extends React.Component {
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: ''};
+  }
+
   render() {
     return <input onChange={(event) => console.log(event.target.value)} />;
   }
