fb7f6df2d40b98930305a78eb35b2a5a2950d91e
Nick Duncan <nickeduncan@gmail.com>
2017-04-29 11:47:13 -0400

    fixed bug in container

--- ReduxSimpleStarter/src/containers/book-list.js
+++ ReduxSimpleStarter/src/containers/book-list.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { select } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
-export default class Booklist extends Component {
+class Booklist extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li
+          key={book.title}
+          className="list-group-item">
+          {book.title}
+        </li>
       );
     });
   }
@@ -17,3 +24,17 @@ export default class Booklist extends Component {
     )
   }
 }
+
+function mapStateToProps(state) {
+  // Whatever is returned will show up as props
+  // inside of BookList
+  return {
+    books: state.books
+  };
+}
+
+function mapDispatchToProps(state) {
+  return bindActionCreators({ selectBook: selectBook }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
