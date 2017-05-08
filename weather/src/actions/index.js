835505edc63dd6a6b5566193ff8af50058336297
Nick Duncan <nickeduncan@gmail.com>
2017-05-07 23:50:31 -0400

    added axios and began using openweathermap api in search

--- weather/src/actions/index.js
+++ weather/src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = 'f361d6815f8d5caf87b6ec3ff0bf4598';
+const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
