7bbd54dcbed00b109164cd7943ba1020a0511c9f
Nick Duncan <nickeduncan@gmail.com>
2017-05-08 11:32:59 -0400

    connected searchbar to action to create ajax request for weather data

--- weather/src/actions/index.js
+++ weather/src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'f361d6815f8d5caf87b6ec3ff0bf4598';
-const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
