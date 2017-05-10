f6c7e10b26ccb7bfd5f4fb1b03d4264ec4fc864c
Nick Duncan <nickeduncan@gmail.com>
2017-05-10 15:01:11 -0400

    added validation errors for the form

--- router/src/components/posts_new.js
+++ router/src/components/posts_new.js
@@ -19,13 +19,13 @@ class PostsNew extends Component {
     return (
       <form>
         <Field
-          label="Title"
+          label="Title For Post"
           name="title"
           component={this.renderField}
         />
         <Field
-          label="Tags"
-          name="tags"
+          label="Categories"
+          name="categories"
           component={this.renderField}
         />
         <Field
@@ -38,6 +38,26 @@ class PostsNew extends Component {
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  // validate the inputs from 'values'
+  if (!values.title) {
+    errors.title = "Enter a title!";
+  }
+  if (!values.categories) {
+    errors.categories = "Enter some categories";
+  }
+  if (!values.content) {
+    errors.content = "Enter some content please";
+  }
+  
+  // If errors is empty, the form is fine to submit
+  // If errors has *any* properties, redux form assumes form is invalid
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
