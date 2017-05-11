12203f7da43da109b892e6d6bf284e9a1abea498
Nick Duncan <nickeduncan@gmail.com>
2017-05-10 20:14:28 -0400

    added post show component with route

---
+++ router/src/components/posts_show.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class PostsShow extends Component {
+  render() {
+    return (
+      <div>
+        Posts Show!
+      </div>
+    );
+  }
+}
