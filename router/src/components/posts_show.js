9066f84b0b9398c9fd6a4097510522dbafd1ba6e
Nick Duncan <nickeduncan@gmail.com>
2017-05-11 09:12:59 -0400

    posts show route displays individual posts

--- router/src/components/posts_show.js
+++ router/src/components/posts_show.js
@@ -9,17 +9,24 @@ class PostsShow extends Component {
   }
 
   render() {
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>;
+    }
 
     return (
       <div>
-        Posts Show!
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
   }
 }
 
 function mapStateToProps({ posts }, ownProps) {
-  return { post: post[ownProps.match.params.id] }
+  return { post: posts[ownProps.match.params.id] };
 }
 
 export default connect(mapStateToProps, { fetchPost })(PostsShow);
