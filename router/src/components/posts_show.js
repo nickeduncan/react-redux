82ea82ef5cb7c176d1b57ce2a6316ad54f52ae06
Nick Duncan <nickeduncan@gmail.com>
2017-05-10 20:24:33 -0400

    fixed bug in show post navigation

--- router/src/components/posts_show.js
+++ router/src/components/posts_show.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class PostsShow extends Component {
+class PostsShow extends Component {
   render() {
     return (
       <div>
@@ -9,3 +9,5 @@ export default class PostsShow extends Component {
     );
   }
 }
+
+export default PostsShow;
