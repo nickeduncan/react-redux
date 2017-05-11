a26a4ee73c45984c389e209f02a768b3feaf6237
Nick Duncan <nickeduncan@gmail.com>
2017-05-10 23:05:57 -0400

    added ownprops

--- router/src/components/posts_show.js
+++ router/src/components/posts_show.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
   render() {
+
     return (
       <div>
         Posts Show!
@@ -10,4 +18,8 @@ class PostsShow extends Component {
   }
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps) {
+  return { post: post[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
